bc5153b0ee8717a3c5a1862b383342bd24651d09
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-11-23 17:01:29 +0800

    add oepngl version and vendor information

--- Demo/glwidget.cpp
+++ Demo/glwidget.cpp
@@ -53,6 +53,17 @@ void GLWidget::initializeGL()
 {
     initializeOpenGLFunctions();
 
+    // 获取OpenGL版本号和厂商信息
+    const GLubyte *name = glGetString(GL_VENDOR);
+    const GLubyte *label = glGetString(GL_RENDERER);
+    const GLubyte *glVersion = glGetString(GL_VERSION);
+    const GLubyte *gluVersion = glGetString(GLU_VERSION);
+
+    printf("OpenGL实现厂商的名字：%s\n",name);
+    printf("渲染器标识符：%s\n",label);
+    printf("OpenGL实现的版本号：%s\n",glVersion);
+    printf("GLU工具库版本：%s\n",gluVersion);
+
     glClearColor(0.7f,0.7f,0.7f,1.0f);
     glEnable(GL_DEPTH_TEST);
 }
