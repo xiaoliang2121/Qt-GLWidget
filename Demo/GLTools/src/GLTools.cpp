899c6d806735497cceccfa6728fde3040d097363
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-11-23 23:16:56 +0800

    fix gltools bug

--- Demo/GLTools/src/GLTools.cpp
+++ Demo/GLTools/src/GLTools.cpp
@@ -1055,7 +1055,7 @@ GLbyte* gltReadBMPBits(const char *szFileName, int *nWidth, int *nHeight)
 		{
 		free(pBitmapInfo);
 		fclose(pFile);
-		return false;
+        return NULL;
 		}
 
 	// Save the size and dimensions of the bitmap
@@ -1067,7 +1067,7 @@ GLbyte* gltReadBMPBits(const char *szFileName, int *nWidth, int *nHeight)
 	if(pBitmapInfo->header.bits != 24)
 		{
 		free(pBitmapInfo);
-		return false;
+        return NULL;
 		}
 
 	if(lBitSize == 0)
